28aa188c5767d67cdc579f6bfa0b495516c18efe
Eric Ellingson <erice@unityworksmedia.com>
2017-05-31 15:32:18 +0900

    added install/init script

--- data/init_db.php
+++ data/init_db.php
@@ -1,5 +1,8 @@
 <?php
 $dbfpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . "rss.db3";
+if (!file_exists($dbfpath)) {
+	file_put_contents($dbfpath, "");
+}	
 $dbfilesize = filesize($dbfpath);
 if ($dbfilesize === 0) {
 	$sql = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . "init_db.sql");
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing..."
+echo "...Initializing database..."
+php data/init_db.php
+echo "...Installed!"
